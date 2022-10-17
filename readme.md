c8788329ffc40469990f5e6d85f7b905bad9dc22
Marius <marius@example.com>
2022-10-17 11:34:57 +0300

    uzd

--- readme.md
+++ readme.md
@@ -15,4 +15,4 @@
 3. SPLICE (kiekvienas pokytis su nepakeistu masyvu) let sent1 = [ 'Sveiki', 'mano', 'vardas', 'James' ]
    3.1. istrinti 'mano'
    3.2. vietoj 'Sveiki' iterpti "Laba diena"
-   3. tarp 'vardas' ir 'James' iterpti 
+   3.3 tarp 'vardas' ir 'James' iterpti 
---
+++ uzd/music.js
@@ -0,0 +1,24 @@
+'use strict';
+console.log('music.js');
+
+// 1. Pabandykime atlikti 5 masyvo (Array) operacijas.
+// 6. Pridėkite į masyvo pradžią „Rap“ ir „Reggae“ į masyvą.
+
+// 2. Sukurkite masyvą (Array) styles su elementais „Jazz“ ir „Blues“.
+let music = ['Jazz', 'Blues'];
+console.log('music ===', music);
+
+// 3. Masyvo (Array) pabaigoje pridėkite „Rock-n-Roll“.
+music.push('Rock-n-Roll');
+console.log('music ===', music);
+
+// 4. Viduryje esančią reikšmę pakeiskite „Classics“.
+music[1] = 'Classics';
+console.log('music ===', music);
+
+// 5. Panaikinkite pirmą masyvo (Array) elementą ir parodykite jį.
+let istrintasPirmas = music.shift();
+console.log('istrintasPirmas ===', istrintasPirmas);
+console.log('music ===', music);
+// alert(istrintasPirmas);
+document.getElementById('title').textContent = istrintasPirmas;
