6de4c3de7b2f7aa4e61955fe52c98dd4b1049d0c
Marius <marius@example.com>
2022-10-17 14:03:39 +0300

    concat

--- uzd/splice.js
+++ uzd/splice.js
@@ -2,10 +2,15 @@
 console.log('splice.js');
 
 // 3. SPLICE (kiekvienas pokytis su nepakeistu masyvu)
+// index          0      1         2        3
 let sent1 = ['Sveiki', 'mano', 'vardas', 'James'];
 
 //    3.1. istrinti 'mano'
-
+// sent1.splice(1, 1);
+// console.log('sent1 ===', sent1);
 //    3.2. vietoj 'Sveiki' iterpti "Laba diena"
-
+// sent1.splice(0, 1, 'Laba diena');
+// console.log('sent1 ===', sent1);
 //    3.3 tarp 'vardas' ir 'James' iterpti 'yra'
+sent1.splice(3, 0, 'yra');
+console.log('sent1 ===', sent1);
